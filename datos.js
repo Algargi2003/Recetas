3ed8d8c25a33adf02d30346d3d8b26bf4f48f714
alvaro <alvarogarciagimeno@gmail.com>
2022-11-29 18:21:34 +0100

    Commit principal

--- datos.js
+++ datos.js
@@ -1,6 +1,68 @@
 let listaRecetas = [
     {
         "id": 1,
+        "nombreBuscar": "galletas",
+        "nombre": "Galletas",
+        "descripcion": "Buena",
+        "tiempoPreparacion": 60,
+        "tiempoCocina": 20,
+        "personas": 4,
+        "ingredientes": [
+            {
+                "cantidad": "250gr",
+                "nombre": "Mantequilla temp. ambiente"
+            },
+            {
+                "cantidad": "250gr",
+                "nombre": "Azúcar"
+            },
+            {
+                "cantidad": "1",
+                "nombre": "Huevo"
+            },
+            {
+                "cantidad": "650gr",
+                "nombre": "Harina"
+            },
+            {
+                "cantidad": "1 cdta",
+                "nombre": "Aroma de vainilla"
+            },
+            {
+                "cantidad": "1 chorrito",
+                "nombre": "Leche"
+            },
+            {
+                "cantidad": "Un poco",
+                "nombre": "Canela"
+            }
+        ],
+        "instrucciones": [
+            {
+                "id": 1,
+                "info": "En un bol batimos la mantequilla con el azúcar"
+            },
+            {
+                "id": 2,
+                "info": "Añadimos el aroma de vainilla, la canela y el huevo"
+            },
+            {
+                "id": 3,
+                "info": "Incorporamos la harina cucharada a cucharada, y si se desmiga echamos un chorrito de leche"
+            },
+            {
+                "id": 4,
+                "info": "Dejamos reposar la masa estirada en la nevera 3 horas"
+            },
+            {
+                "id": 5,
+                "info": "Hacemos las formitas y hornear a 180º"
+            }
+        ]
+    },
+    {
+        "id": 2,
+        "nombreBuscar": "tortitas",
         "nombre": "Tortitas",
         "descripcion": "Buena",
         "tiempoPreparacion": 25,
@@ -10,6 +72,22 @@ let listaRecetas = [
             {
                 "cantidad": "200gr",
                 "nombre": "Harina"
+            },
+            {
+                "cantidad": "4",
+                "nombre": "Huevos"
+            },
+            {
+                "cantidad": "4",
+                "nombre": "Huevos"
+            },
+            {
+                "cantidad": "4",
+                "nombre": "Huevos"
+            },
+            {
+                "cantidad": "4",
+                "nombre": "Huevos"
             },
             {
                 "cantidad": "4",
@@ -24,8 +102,9 @@ let listaRecetas = [
         ]
     },
     {
-        "id": 2,
-        "nombre": "Galletas",
+        "id": 3,
+        "nombreBuscar": "arroz",
+        "nombre": "Arroz",
         "descripcion": "Buena",
         "tiempoPreparacion": 25,
         "tiempoCocina": 10,
@@ -34,6 +113,10 @@ let listaRecetas = [
             {
                 "cantidad": "200gr",
                 "nombre": "Harina"
+            },
+            {
+                "cantidad": "4",
+                "nombre": "Huevos"
             },
             {
                 "cantidad": "4",
--- js/app.js
+++ js/app.js
@@ -1,23 +1,19 @@
+function sortArray(x,y){
+    if(x.nombreBuscar<y.nombreBuscar){return -1;};
+    if(x.nombreBuscar>y.nombreBuscar){return 1;};
+    return 0;
+}
+
 function pintaArticulos() {
     let contenedor = document.getElementById("contenedor");
     let texto = "";
 
-    listaRecetas.forEach((element) => {
-        // texto +=
-        //     "<div class='col'><div class='card'><img src='./assets/" +
-        //     element.id +
-        //     ".jpg' class='card-img-top'>||<div class='card-body'> <h5 class='card-title'>" +
-        //     element.nombre +
-        //     "</h5><p class='card-text'>" +
-        //     element.descripcion +
-        //     "</p><b><p class='card-text text-center'>" +
-        //     element.precio +
-        //     "€</p></b></div><button id='" +
-        //     element.codigo +
-        //     "'  class='btn-success'>comprar</button></div></div>";
+    let nuevalistaRecetas=listaRecetas.sort(sortArray);
+    nuevalistaRecetas.forEach((element) => {
+        
         texto += `
         <div class="col">
-            <div class="card">
+            <div class="card ">
                 <img src="./assets/img/${element.nombre}.jpg" class="card-img-top">
                 <div class="card-body">
                     <h5 class="card-title">${element.nombre}</h5>
@@ -35,16 +31,13 @@ function pintaArticulos() {
         texto += `</ol>
                 <p class="card-text"><b>Preparación:</b></p>
                 <ol>`;
-        element.instrucciones.forEach(x=>{
-            texto+=`
+        element.instrucciones.forEach((x) => {
+            texto += `
                 <li>${x.info}</li>
             `;
-        })
+        });
 
-
-
-
-             texto+=   `
+        texto += `
                 </div>
             </div>
         </div>
@@ -54,37 +47,34 @@ function pintaArticulos() {
     contenedor.innerHTML = texto;
 }
 
-function obtenerBuscada(){
-    let busqueda = document.getElementById("busqueda").value;
+function obtenerBuscada() {
+    let busqueda = document.getElementById("busqueda").value.toLowerCase();
     let f;
     let cosa;
-    listaRecetas.forEach(element=>{
-        if(element.nombre==busqueda){
-            f= element;
+    listaRecetas.forEach((element) => {
+        if (element.nombreBuscar == busqueda) {
+            f = element;
         }
-        
-    })
-    if(busqueda=="todos"){
+    });
+    if (busqueda == "todos") {
         pintaArticulos();
+    } else if(f == undefined){
+        alert("La receta no existe");
     }else{
         verSoloUna(f.id);
     }
     
-    
-    
 }
-
 
 function verSoloUna(idBuscar) {
     let contenedor = document.getElementById("contenedor");
-    let existe = listaRecetas.indexOf(listaRecetas[idBuscar]);
-    let buscada = listaRecetas.find(e=>e.id==idBuscar);
+    let existe = listaRecetas.findIndex(e=> e.id == idBuscar);
+    let buscada = listaRecetas.find((e) => e.id == idBuscar);
     let texto;
-    if (existe == -1) {
-        alert("Esta receta no existe");
-    } else {
+    
+    
         texto += `
-        <div class="col">
+        <div class="col ">
             <div class="card ">
                 <img src="./assets/img/${buscada.nombre}.jpg" class="card-img-top">
                 <div class="card-body">
@@ -92,7 +82,7 @@ function verSoloUna(idBuscar) {
                     <p class="card-text"><b>Ingredientes:</b></p>
                     <ol>`;
 
-                    buscada.ingredientes.forEach((i) => {
+        buscada.ingredientes.forEach((i) => {
             texto += `
             
                 <li>${i.nombre}: ${i.cantidad}</li>
@@ -101,13 +91,21 @@ function verSoloUna(idBuscar) {
         });
 
         texto += `</ol>
-                </div>
+                <p class="card-text"><b>Preparación:</b></p>
+                <ol>`;
+        buscada.instrucciones.forEach((x) => {
+            texto += `
+                <li>${x.info}</li>
+            `;
+        });
+                
+        texto+=`</div>
             </div>
         </div>
         `;
-        contenedor.innerHTML=texto;
+        contenedor.innerHTML = texto;
     }
-}
+
 
 window.onload = () => {
     pintaArticulos();
