2f384b46a5701ab3d16e24d054e2812f506ee824
alvaro <alvarogarciagimeno@gmail.com>
2022-11-29 21:17:25 +0100

    Añadir busqueda por botones y boton todos

--- js/app.js
+++ js/app.js
@@ -15,27 +15,13 @@ function pintaArticulos() {
         <div class="col">
             <div class="card ">
                 <img src="./assets/img/${element.nombre}.jpg" class="card-img-top">
-                <div class="card-body">
+                <div class="card-body prueba">
                     <h5 class="card-title">${element.nombre}</h5>
-                    <p class="card-text"><b>Ingredientes:</b></p>
-                    <ol>`;
-
-        element.ingredientes.forEach((i) => {
-            texto += `
-            
-                <li>${i.nombre}: ${i.cantidad}</li>
-            
-            `;
-        });
-
-        texto += `</ol>
-                <p class="card-text"><b>Preparación:</b></p>
-                <ol>`;
-        element.instrucciones.forEach((x) => {
-            texto += `
-                <li>${x.info}</li>
-            `;
-        });
+                    <p class="card-text"><b>Descripcion:</b></p>
+                    <p class=" card-text">${element.descripcion}</p>
+                    
+                    <button id='${element.id}' class="btn-info ">Más info</button>
+                    `;
 
         texto += `
                 </div>
@@ -45,10 +31,28 @@ function pintaArticulos() {
     });
 
     contenedor.innerHTML = texto;
+    ponFuncionBoton();
 }
-
-function obtenerBuscada() {
+function ponFuncionBoton() {
+    let boton = document.getElementsByClassName("btn-info");
+    let botonesArray = [...boton];
+    botonesArray.forEach((e) => {
+        let receta = listaRecetas.find((i) => i.id == e.id);
+        e.addEventListener("click", function () {
+            buscar(receta.nombreBuscar);
+        });
+    });
+    let botonTodos = document.getElementById("botonTodos");
+    botonTodos.onclick = () =>{
+        pintaArticulos();
+    };
+}
+function obtenerBuscada(){
     let busqueda = document.getElementById("busqueda").value.toLowerCase();
+    buscar(busqueda);
+}
+function buscar(busqueda) {
+    
     let f;
     let cosa;
     listaRecetas.forEach((element) => {
@@ -79,8 +83,15 @@ function verSoloUna(idBuscar) {
                 <img src="./assets/img/${buscada.nombre}.jpg" class="card-img-top">
                 <div class="card-body">
                     <h5 class="card-title">${buscada.nombre}</h5>
+                    <label>Comensales: 
+                    <input type="number" placeholder="${buscada.personas}" id="${buscada.id}pers" class="ancho-comensales"></input><button id="boton-pers">H</button>
                     <p class="card-text"><b>Ingredientes:</b></p>
                     <ol>`;
+        // let botonComensales=document.getElementById("boton-pers");
+        // botonComensales.onclick = () =>{
+        //     let infor = document.getElementById(buscada.id+"pers").value;
+        //     console.log(infor);
+        // };
 
         buscada.ingredientes.forEach((i) => {
             texto += `
